84d5d59afdf5c3757a1e684742f93ce7371f1bdd
nheath99 <njh86@oulook.com>
2018-09-27 16:32:41 +0100

    More to #6

--- src/BlazorNodaTimeDateTimePicker/DatePickerState.cs
+++ src/BlazorNodaTimeDateTimePicker/DatePickerState.cs
@@ -5,9 +5,18 @@ namespace BlazorNodaTimeDateTimePicker
 {
     public class DatePickerState
     {
+		public DatePickerState()
+		{
+			Today = SystemClock.Instance.Today();
+			MonthToDisplay = Today.StartOfMonth();
+		}
+
+		public LocalDate Today { get; }
+		
 		public string Val { get; set; } = "123";
 		public LocalDate? SelectedDate { get; private set; }
 		public ViewMode ViewMode { get; private set; } = ViewMode.Days;
+		public LocalDate MonthToDisplay { get; set; }
 
 		public event Action OnSelected;
 		public event Action OnUpdated;
@@ -23,8 +32,25 @@ namespace BlazorNodaTimeDateTimePicker
 		void Opened() => OnOpened?.Invoke();
 		void Closed() => OnClosed?.Invoke();
 		void Cleared() => OnCleared?.Invoke();
-		void Today() => OnToday?.Invoke();
+		void TodaySelected() => OnToday?.Invoke();
 		void StateChanged() => OnStateChanged?.Invoke();
+
+		internal void SetSelectedDate(LocalDate selectedDate)
+		{
+			//SelectedDate = selectedDate;
+			if (SelectedDate != selectedDate)
+			{
+				SelectedDate = selectedDate;
+
+				if (selectedDate.Month < MonthToDisplay.Month)
+					PreviousMonth();
+				else if (selectedDate.Month > MonthToDisplay.Month)
+					NextMonth();
+
+				StateChanged();
+				Selected();
+			}
+		}
 
 		internal void SetViewMode(ViewMode viewMode)
 		{
@@ -69,6 +95,16 @@ namespace BlazorNodaTimeDateTimePicker
 				default:
 					break;
 			}
+		}
+
+		internal void NextMonth()
+		{
+			MonthToDisplay = MonthToDisplay.PlusMonths(1);
+		}
+
+		internal void PreviousMonth()
+		{
+			MonthToDisplay = MonthToDisplay.PlusMonths(-1);
 		}
 	}
 }
